83883536805e11d31b2c7de3495f5420b8775da1
Sid Kothari <sid.kothari7@gmail.com>
2024-02-13 09:51:35 -0500

    Clean up code; set css variables for styling; add button hover and click styling; refactor script to extract css properties instead of redefining them; refactor script to include start and quit buttons inside game and display controllers

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-//  GLOBAL VARIABLES {{{
-const startBtn = document.querySelector('button.start');
-const quitBtn = document.querySelector('button.quit');
-const game = GameController();
-//}}}
-
-// FUNCTIONS {{{
-function GameBoard() {//{{{
-  const board = Array(9).fill(' ');
+// FUNCTIONS 
+function GameBoard() {
+  //{{{
+  const board = Array(9).fill(" ");
 
   const addToken = (token, position) => {
-    if (board[position] === ' ') {
+    if (board[position] === " ") {
       board[position] = token;
-      return 0
-    } else { return 1 }
-  }
+      return 0;
+    } else {
+      return 1;
+    }
+  };
 
   const gameState = (token) => {
     const matchStr = new RegExp(`${token}{3}`);
@@ -25,211 +22,289 @@ function GameBoard() {//{{{
       [board[1], board[4], board[7]],
       [board[2], board[5], board[8]],
       [board[0], board[4], board[8]],
-      [board[2], board[4], board[6]]
+      [board[2], board[4], board[6]],
     ];
 
-    if (winningSets.some(winningSet => matchStr.test(winningSet.join('')))) { return 1 }
-    else if (!board.some(cell => cell === ' ')) { return 2 }
-    else { return 0 }
-  }
+    if (winningSets.some((winningSet) => matchStr.test(winningSet.join("")))) {
+      return 1;
+    } else if (!board.some((cell) => cell === " ")) {
+      return 2;
+    } else {
+      return 0;
+    }
+  };
 
   const resetBoard = () => {
-    board.fill(' ');
-  }
+    board.fill(" ");
+  };
 
-  const getBoard = () => {
-    return board;
-  }
+  const getBoard = () => board;
 
-  const viewBoard = () => {
+  const viewBoard = () => {//{{{
     let index = [0, 3, 6];
-    console.log('-----------------', '   ', '-----------------');
+    console.log("-----------------", "   ", "-----------------");
     for (let i of index) {
-      console.log(' ', board[i], ' | ', board[i + 1], ' | ', board[i + 2], '       ', i + 1, ' | ', i + 2, ' | ', i + 3, ' ');
-    console.log('-----------------', '   ', '-----------------');
+      console.log(
+        " ",
+        board[i],
+        " | ",
+        board[i + 1],
+        " | ",
+        board[i + 2],
+        "       ",
+        i + 1,
+        " | ",
+        i + 2,
+        " | ",
+        i + 3,
+        " ",
+      );
+      console.log("-----------------", "   ", "-----------------");
     }
-  }
+  };//}}}
 
   return {
     addToken,
     gameState,
     getBoard,
-    resetBoard
-  }
-}//}}}
+    resetBoard,
+  };
+} //}}}
 
-function Player(name, token) {//{{{
-  
-  const viewPlayer = () => {
-    return `${name} is playing as ${token}`;
-  }
+function Player(name, token) {
+  //{{{
+  let points = 0;
+
+  const getPoints = () => points;
+  const givePoints = () => points++;
 
   return {
     name,
     token,
-    viewPlayer
-  }
-}//}}}
+    getPoints,
+    givePoints
+  };
+} //}}}
 
-function DisplayController() {//{{{
-  const DOMBoard = document.querySelector('div.gameboard');
-  // const DOMPanelLeft = document.querySelector('div.left');
-  // const DOMPanelRight = document.querySelector('div.right');
-  const DOMPlayerTurn = document.querySelector('h3.turn');
-  const DOMGameLogTitle = document.querySelector('h3.gamelog-title');
-  const DOMGameLog = document.querySelector('div.gamelog');
-  const DOMGameLogAnchor = document.querySelector('#gamelog-anchor');
+function DisplayController() {
+  //{{{
+  const DOMBoard = document.querySelector("div.gameboard");
+  const DOMPanelLeft = document.querySelector('div.left');
+  const DOMPanelRight = document.querySelector('div.right');
+  const DOMScoreBox = DOMPanelLeft.querySelector('div.scoreboard-box');
+  const DOMNotification = document.querySelector("h3.notification");
+  const DOMGameLog = document.querySelector("div.gamelog");
+  const DOMGameLogAnchor = document.querySelector("#gamelog-anchor");
+  const DOMstartBtn = document.querySelector("button.start");
+  const DOMquitBtn = document.querySelector("button.quit");
 
-  const newGame = (player1, player2, gameBoard) => {
-    updateBoard(gameBoard);
-    updateGameLog(`NEW GAME`, 1);
-    updateGameLog(player1.viewPlayer() + ' and ' + player2.viewPlayer(), 2);
-    updatePlayerTurn(player1);
-    DOMGameLogTitle.textContent = 'Game Log';
-    DOMGameLog.classList.add('live');
-    DOMGameLogTitle.classList.add('live');
-  }
+  const rootEl = document.documentElement;
+  const notificationDelay = parseInt(getComputedStyle(rootEl).getPropertyValue('--notification-delay').match(/\d+/)[0]);
+  const fadeOutDelay = parseInt(getComputedStyle(rootEl).getPropertyValue('--fade-delay').match(/\d+/)[0]);
+  const gameQuitDelay = 1000;
 
-  const quitGame = (currentPlayer, gameBoard) => {
-    updateGameLog(`Awe, ${currentPlayer.name} quit the game! What a coward...`, 1);
-    setTimeout(() => {
-      updateBoard(gameBoard);
-      updatePlayerTurn();
-      DOMGameLog.replaceChildren(DOMGameLogAnchor);
-      DOMGameLogTitle.textContent = '';
-      DOMGameLog.classList.remove('live');
-      DOMGameLogTitle.classList.remove('live');
-    }, '1800');
-  }
-
-  const gameOver = (currentPlayer, gameState) => {
-    if (gameState === 1) {
-      updateGameLog(`${currentPlayer.name} wins!!!`, 1);
-    } else if (gameState === 2) {
-      updateGameLog("Looks like it's a tie...", 1);
-    }
-    updatePlayerTurn();
-  }
-
-  const updateBoard = (gameBoard) => {
-    const board = gameBoard.getBoard();
-    const newDOMBoard = [];
-    for (let i = 0; i < board.length; i++) {
-      const gameSquare = document.createElement('button');
-      const squareContent = document.createElement('span');
-      gameSquare.classList.add('game-square');
-      gameSquare.dataset.index = i;
-      squareContent.textContent = board[i];
-      gameSquare.appendChild(squareContent);
-      newDOMBoard.push(gameSquare);
-    }
-    DOMBoard.replaceChildren(...newDOMBoard);
+// PRIVATE METHODS {{{
+  const delay = (time) => {
+      return new Promise((resolve, reject) => setTimeout(resolve, time));
   }
 
   const updateGameLog = (message, importance) => {
-    const newLog = document.createElement('p');
-    newLog.classList.add('log');
+    const newLog = document.createElement("p");
+    newLog.classList.add("log");
     if (importance === 1) {
-      newLog.classList.add('important');
+      newLog.classList.add("important");
     }
     newLog.textContent = `${message}`;
     DOMGameLog.insertBefore(newLog, DOMGameLogAnchor);
     DOMGameLogAnchor.scrollIntoView();
-  }
+  };
+//}}}
+
+// PUBLIC METHODS {{{
+  const init = (gameBoard) => {
+    const board = gameBoard.getBoard();
+    const newDOMBoard = [];
+    for (let i = 0; i < board.length; i++) {
+      const gameSquare = document.createElement("button");
+      const squareContent = document.createElement("span");
+      gameSquare.classList.add("game-square");
+      gameSquare.dataset.index = i;
+      gameSquare.appendChild(squareContent);
+      newDOMBoard.push(gameSquare);
+    }
+    DOMBoard.replaceChildren(...newDOMBoard);
+
+    DOMquitBtn.disabled = true;
+  };
+
+  const newGame = async (player1, player2, gameBoard) => {
+    updateBoard(gameBoard);
+    updateGameLog(`NEW GAME`, 1);
+    updateGameLog(`${player1.name} is playing as ${player1.token} and ${player2.name} is playing as ${player2.token}`)
+    await updateNotification(`${player1.name}'s turn...`);
+    DOMPanelRight.classList.add('live');
+    DOMPanelLeft.classList.add('live');
+    DOMstartBtn.textContent = 'Restart';
+    DOMquitBtn.disabled = false;
+  };
+
+  const quitGame = (currentPlayer, gameBoard) => {
+    const liveGameSquares = DOMBoard.querySelectorAll('button > span.live');
+    updateGameLog(
+      `Awe, ${currentPlayer.name} quit the game! What a coward...`,
+      1,
+    );
+    delay(gameQuitDelay).then(() => {
+      liveGameSquares.forEach(el => el.classList.remove('live'));
+      DOMPanelRight.classList.remove('live');
+      DOMPanelLeft.classList.remove('live');
+      return delay(fadeOutDelay);
+    }).then(() => {
+      DOMstartBtn.textContent = 'Start';
+      DOMquitBtn.disabled = true;
+      updateNotification('');
+      DOMGameLog.replaceChildren(DOMGameLogAnchor);
+      updateBoard(gameBoard);
+    });
+  };
+
+  const gameOver = (currentPlayer, gameState) => {
+    if (gameState === 1) {
+      updateGameLog(`${currentPlayer.name} wins!!!`, 1);
+      updateNotification('Winner!');
+    } else if (gameState === 2) {
+      updateGameLog("Looks like it's a tie...", 1);
+      updateNotification('Tie Game');
+    }
+    DOMstartBtn.textContent = 'New Game';
+  };
+
+  const updateBoard = (gameBoard) => {
+    const board = gameBoard.getBoard();
+    for (let i = 0; i < board.length; i++) {
+      const gameSquare = document.querySelector(`[data-index="${i}"]`);
+      const squareContent = gameSquare.firstChild;
+      squareContent.textContent = board[i];
+      if (!(board[i] === " ")) {
+        squareContent.classList.add("live");
+      }
+    }
+  };
 
   const updatePlayerMove = (currentPlayer, playerMove) => {
-    updateGameLog(`${currentPlayer.name} played square ${parseInt(playerMove) + 1}!`, 2);
-  }
+    updateGameLog(
+      `${currentPlayer.name} played square ${parseInt(playerMove) + 1}!`,
+      2
+    );
+  };
 
-  const updatePlayerTurn = (currentPlayer = '') => {
-    if (currentPlayer) {
-      DOMPlayerTurn.textContent = `${currentPlayer.name}'s turn...`
-    } else {
-      DOMPlayerTurn.textContent = '';
-    }
-  }
+  const updateNotification = async (message) => {
+    DOMNotification.classList.remove("transition");
+    return delay(notificationDelay).then(() => {
+      DOMNotification.textContent = message;
+      DOMNotification.classList.add("transition");
+    });
+  };
+//}}}
 
   return {
+    init,
     newGame,
     quitGame,
     gameOver,
     updateBoard,
     updatePlayerMove,
-    updatePlayerTurn
-  }
-}//}}}
+    updateNotification
+  };
+} //}}}
 
-function GameController() {//{{{
+const game = (function GameController() {
+  //{{{
   let gameBoard;
   let player1;
   let player2;
   let currentPlayer;
+  let playerQuit = true;
   let gameState;
 
-  const DOMBoard = document.querySelector('div.gameboard');
-  const displayControl = DisplayController();
+  const startBtn = document.querySelector("button.start");
+  const quitBtn = document.querySelector("button.quit");
+  const DOMBoard = document.querySelector("div.gameboard");
+  const display = DisplayController();
 
-  const initBoard = () => {
+// PRIVATE METHODS {{{
+  const init = () => {
     gameBoard = GameBoard();
-    displayControl.updateBoard(gameBoard);
+    display.init(gameBoard);
+    startBtn.addEventListener("click", () => {
+      newGame();
+    });
+    quitBtn.addEventListener("click", () => {
+      quitGame();
+    });
+  };
+
+  const playRound = async (e) => {
+    if (!e.target.classList.contains("game-square") || gameState) {
+      return;
+    }
+
+    const playerMove = e.target.dataset.index;
+    const invalidMove = gameBoard.addToken(currentPlayer.token, playerMove);
+    if (invalidMove) {
+      return;
+    }
+
+    display.updateBoard(gameBoard);
+    display.updatePlayerMove(currentPlayer, playerMove);
+
+    gameState = gameBoard.gameState(currentPlayer.token);
+    if (gameState) {
+      display.gameOver(currentPlayer, gameState);
+      if (gameState === 1) { currentPlayer.givePoints() }
+    } else {
+      currentPlayer = currentPlayer === player1 ? player2 : player1;
+      await display.updateNotification(`${currentPlayer.name}'s turn...`);
+    }
   }
 
-  const quitGame = async () => {
-    DOMBoard.removeEventListener('click',  playRound);
+  const beginGame = () => {
     gameBoard.resetBoard();
-    await displayControl.quitGame(currentPlayer, gameBoard);
+    currentPlayer = player1;
+    gameState = 0;
+    display.newGame(player1, player2, gameBoard);
+  }
+//}}}
+
+// PUBLIC METHODS {{{
+  const quitGame = () => {
+    DOMBoard.removeEventListener("click", playRound);
+    gameBoard.resetBoard();
+    display.quitGame(currentPlayer, gameBoard);
     player1 = null;
     player2 = null;
     currentPlayer = null;
     gameState = null;
-  }
+    playerQuit = true;
+  };
 
   const newGame = () => {
-    DOMBoard.addEventListener('click', playRound);
-    gameBoard.resetBoard();
-    player1 = Player('Player 1', 'X');
-    player2 = Player('Player 2', 'O');
-    currentPlayer = player1;
-    gameState = 0;
-    displayControl.newGame(player1, player2, gameBoard);
-  }
-
-  const playRound = (e) => {
-    if (!e.target.classList.contains('game-square') || gameState) { return }
-
-    const playerMove = e.target.dataset.index;
-    const invalidMove = gameBoard.addToken(currentPlayer.token, playerMove);
-
-    if (invalidMove) {return}
-
-    displayControl.updateBoard(gameBoard);
-    displayControl.updatePlayerMove(currentPlayer, playerMove);
-
-    gameState = gameBoard.gameState(currentPlayer.token);
-    if (gameState) {
-      displayControl.gameOver(currentPlayer, gameState);
-    } else {
-      currentPlayer = currentPlayer === player1 ? player2 : player1;
-      displayControl.updatePlayerTurn(currentPlayer);
+    if (playerQuit) {
+      DOMBoard.addEventListener("click", playRound);
+      player1 = Player("Player 1", "X");
+      player2 = Player("Player 2", "O");
+      playerQuit = false;
     }
-  }
+    beginGame();
+  };
+//}}}
 
-  initBoard();
-
+  init();
+  
   return {
     newGame,
     quitGame
-  }
+  };
+})();//}}}
 
-}//}}}
-//}}}
 
-// LISTENERS {{{
-startBtn.addEventListener('click', () => {
-  game.newGame();
-})
-
-quitBtn.addEventListener('click', () => {
-  game.quitGame();
-})
-//}}}
